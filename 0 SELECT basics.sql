1a425ea68bccb733b305aa2dadb1f6d8ae7c69a3
PsialeDev <psialedev@gmail.com>
2020-03-19 01:54:54 -0600

    Added the file for the 2nd module

--- 0 SELECT basics.sql
+++ 0 SELECT basics.sql
@@ -1,5 +1,9 @@
 -- #1
-
+    SELECT population FROM world
+  WHERE name = 'Germany'
 -- #2
-
--- #3
+    SELECT name, population FROM world
+  WHERE name IN ('Brazil', 'Russia', 'India', 'China');
+-- #3   
+    SELECT name, area FROM world
+  WHERE area BETWEEN 250000 AND 300000
