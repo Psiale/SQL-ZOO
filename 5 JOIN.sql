5568a8d22e1bd47c39a7a606eb54af1497f41611
Alexis <psialedev@gmail.com>
2020-03-19 16:37:50 -0600

    Working on  the 5th module

--- 5 JOIN.sql
+++ 5 JOIN.sql
@@ -7,22 +7,35 @@ SELECT id,stadium,team1,team2
   FROM game WHERE id  = '1012'
 
 -- # 3
+SELECT player, teamid, stadium, mdate 
+  FROM game JOIN goal ON (id=matchid)
+WHERE teamid LIKE '%GER'
 
 
 -- # 4
-
+SELECT team1, team2, player FROM goal
+JOIN game  ON (id=matchid)
+WHERE player LIKE 'Mario%'
 
 -- # 5
-
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam ON (teamid=id)
+ WHERE gtime<=10
 
 -- # 6
-
+SELECT mdate, teamname FROM game 
+JOIN eteam ON (team1 = eteam.id)
+WHERE coach LIKE '%Santos'
 
 -- # 7
-
+SELECT player FROM goal 
+JOIN game ON (id=matchid)
+WHERE stadium LIKE '%Warsaw'
 
 -- # 8
-
+SELECT DISTINCT player FROM goal
+JOIN game ON (matchid=id AND teamid != 'GER')
+WHERE team1 = 'GER' OR team2 = 'GER'
 
 -- # 9
 
