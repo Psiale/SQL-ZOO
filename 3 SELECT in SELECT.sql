4393dc0fcad9f36cd7ac42ee4e2e44a6b8df696c
Alexis <psialedev@gmail.com>
2020-03-19 11:30:41 -0600

    Finished the 3th module

--- 3 SELECT in SELECT.sql
+++ 3 SELECT in SELECT.sql
@@ -34,13 +34,29 @@ WHERE gdp >  (SELECT MAX(GDP) FROM world
 WHERE continent = 'Europe');
 
 -- #7
-
+SELECT continent, name, area FROM world x
+  WHERE area>= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area> 0);
 
 -- #8
-
+SELECT continent, name FROM world x
+WHERE name =
+    (SELECT name FROM world y  
+    WHERE y.continent = x.continent
+    ORDER BY name LIMIT 1);
 
 -- #9
-
+SELECT name, continent, population FROM world x
+WHERE 25000000 >= ALL
+(SELECT population FROM world y
+WHERE y.continent = x.continent);
 
 -- #10
+SELECT name, continent FROM world x 
+WHERE population > ALL(
+    SELECT (population * 3) FROM world y
+    WHERE y.continent = x.continent
+    AND y.name != x.name)
 
