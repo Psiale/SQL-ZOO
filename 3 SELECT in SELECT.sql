4e983680fa5239573a2e5271de12856dd82ed979
Alexis <psialedev@gmail.com>
2020-03-19 07:57:30 -0600

    working on the third module

---
+++ 3 SELECT in SELECT.sql
@@ -0,0 +1,44 @@
+-- #1
+SELECT name FROM world
+WHERE population > 
+(SELECT population FROM world
+WHERE name = 'Russia')
+
+-- #2
+SELECT name FROM world
+WHERE (gdp/population) > 
+(SELECT (gdp/population) FROM world
+WHERE name = 'United Kingdom') AND  continent = 'Europe';
+
+-- #3
+SELECT name, continent FROM world
+WHERE continent IN (SELECT continent FROM world
+WHERE name IN ('Argentina', 'Australia')) ORDER BY name;
+
+-- #4
+SELECT name, population FROM world
+WHERE population > (SELECT population FROM world
+WHERE name  = 'Canada')
+AND population < (SELECT population FROM world
+WHERE name = 'Poland' );
+
+-- #5
+SELECT name, 
+CONCAT (ROUND (population /(SELECT population FROM world
+WHERE name = 'Germany') * 100),'%') FROM world
+WHERE  continent = 'Europe';
+
+-- #6
+
+
+-- #7
+
+
+-- #8
+
+
+-- #9
+
+
+-- #10
+
