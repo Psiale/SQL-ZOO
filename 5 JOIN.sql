45146423c59bd34af1160093cf8d9e56fa60d159
Alexis <psialedev@gmail.com>
2020-03-19 12:58:40 -0600

    Working on  the 5th module

---
+++ 5 JOIN.sql
@@ -0,0 +1,46 @@
+-- # 1
+SELECT matchid, player FROM goal 
+  WHERE teamid LIKE '%GER'
+
+-- # 2
+SELECT id,stadium,team1,team2
+  FROM game WHERE id  = '1012'
+
+-- # 3
+
+
+-- # 4
+
+
+-- # 5
+
+
+-- # 6
+
+
+-- # 7
+
+
+-- # 8
+
+
+-- # 9
+
+
+-- # 10
+
+
+-- # 11
+
+
+-- # 12
+
+
+-- # 13
+
+
+-- # 14
+
+
+-- # 15
+
